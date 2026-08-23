49d7fd911cf65bc01b1f5b20ff3d4a59b5398ee7
Artsiom Stulba <lizoksame@MacBook-Air13-M1.local>
2026-08-23 16:35:19 +0300

    Add project

---
+++ Done/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Done
+//
+//  Created by LizOk&Same on 23.08.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
